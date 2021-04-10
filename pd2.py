1deb9ef82d9ce928d4ff5a24c4026dca40e4f774
ntpdev <52379467+ntpdev@users.noreply.github.com>
2021-04-10 08:16:51 +0100

    d
     lease enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.

--- pd2.py
+++ pd2.py
@@ -1,9 +1,68 @@
 #!/usr/bin/python3
-from datetime import datetime
-import glob
+from datetime import datetime, timedelta
+import glob as gb
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+
+def rth_index(df):
+    selector = (df.index.minute == 0) & (df.index.to_series().diff() != timedelta(minutes=1))
+    idx = df[selector].index.to_series()
+    # open will always have a gap before it 23:00 CET to add to get RTH hours 14:30 - 20:59
+    idx_open = idx.add(timedelta(hours=15, minutes=30))
+    idx_open = df.index.intersection(pd.Index(idx_open))
+    idx_close = idx.add(timedelta(hours=21, minutes=59))
+    idx_close = df.index.intersection(pd.Index(idx_close))
+    return idx_open, idx_close
+
+def rth_bars(df):
+    idx_open, idx_close = rth_index(df)
+    return aggregate_bars(df, idx_open, idx_close)
+
+# create a new DF which aggregates bars between inclusive indexes
+def aggregate_bars(df, idxs_start, idxs_end):
+    rows = []
+    dts = []
+    for s,e in zip(idxs_start, idxs_end):
+        dts.append(e.date())
+        r = {}
+        r['Open'] = df.Open[s]
+        r['High'] = df.High[s:e].max()
+        r['Low'] = df.Low[s:e].min()
+        r['Close'] = df.Close[e]
+        r['Volume'] = df.Volume[s:e].sum()
+        vwap = np.average( df.WAP[s:e], weights=df.Volume[s:e] )
+        r['VWAP'] = round(vwap, 2)
+        rows.append(r)
+    daily = pd.DataFrame(rows, index=dts)
+    daily['Change'] = daily['Close'].sub(daily['Close'].shift())
+    daily['DayChg'] = daily['Close'].sub(daily['Open'])
+    daily['Range'] = daily['High'].sub(daily['Low'])
+    return daily
+
+def calc_vwap(df):
+    is_first_bar = df.index.to_series().diff() != timedelta(minutes=1)
+    xs = []
+    start = 0
+    for i,r in df.iterrows():
+        if is_first_bar.loc[i]:
+            start = i
+        v = np.average( df['WAP'].loc[start:i], weights=df['Volume'].loc[start:i] )
+        xs.append(round(v, 2))
+    return pd.Series(xs, df.index)
+
+def load_files(fname):
+    dfs = [load_file(e) for e in gb.glob(fname) ]
+    return pd.concat(dfs)
+
+def load_file(fname):
+    df = pd.read_csv(fname, parse_dates=['Date'], index_col='Date')
+    print(f'loaded {fname} {df.shape[0]} {df.shape[1]}')
+    return df
+
+# return boolean index
+def find_price_intersection(df, n, start, end):
+    return (df['Low'].iloc[start:end] <= n) & (df['High'].iloc[start:end] >= n)
 
 def calc_hilo(df):
     idx_end = 2600
@@ -41,13 +100,14 @@ def make_colour(h,l):
     return 'green' if h > abs(l) else 'red'
 
 print(f'Hello world {datetime.now()}')
-#xs = glob.glob('/media/niroo/ULTRA/esh1*')
-xs = glob.glob('D:\esh1*.csv')
-for s in xs:
-    print(s)
-    df = pd.read_csv(s, index_col='Date')
-    print(df)
-    calc_hilo(df)
+#xs = load_files('/media/niroo/ULTRA/esh1*')
+df = load_files('D:\\esh1*.csv')
+df['VWAP'] = calc_vwap(df)
+calc_hilo(df)
+print('--- RTH bars ---')
+df2 = rth_bars(df)
+print(df2)
+
 
 cols = []
 idx_end = 2600
@@ -72,4 +132,4 @@ fig.add_trace(go.Bar(x=df.index[0:idx_end],
     base=df.Low.iloc[0:idx_end],
     marker=dict(color=df.Colour.iloc[0:idx_end]) ))
 #    color_discrete_map={ '0' : 'blue' }))
-fig.show()
+#fig.show()
